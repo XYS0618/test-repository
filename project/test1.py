3ca74f82ffcb42adb28e5e09268495b54a1def09
XYS0618 <19960168478@163.com>
2023-04-18 10:20:15 +0800

    第一次提交

---
+++ project/test1.py
@@ -0,0 +1,28 @@
+import math
+
+# 整数类型
+a = 5
+b = 10
+print(a > b)
+
+# 浮点型
+a = 2.2
+b = 3.2
+print(a + b == 5.4)
+
+# 复数生成
+# Python 内置复数数据类型
+print(math.pi)
+
+# 逻辑类型
+# 与 and
+# 或 or
+# 非 not
+# 优先级
+# not 最高 and次之 or最低
+
+# 各种类型对应的真值
+if 1-1:
+    print("a")
+else:
+    print("b")
